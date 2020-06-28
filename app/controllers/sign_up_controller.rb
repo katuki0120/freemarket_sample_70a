78ae84c86d317d11a12ec3d8d203ee0057f7fe69
yoshiryo413 <yoshida.rio13@gmail.com>
2020-06-28 18:34:54 +0900

    firstcommit

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -1,4 +1,17 @@
 class SignUpController < ApplicationController
   def index
+
+
+    # def new
+    #   @post = Post.new
+    # end
+  
+    # def create
+    #   Post.create(post_params)
+    #   redirect_to posts_path
+    # end
+
+ 
+
   end
 end
