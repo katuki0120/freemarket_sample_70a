f6cdd2046cef6380671b1348d20ee1cf6132f398
katuki0120 <katuki2898@gmail.com>
2020-07-05 18:28:19 +0900

    editとupadateメソッドの追記（chat-space参考)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,16 +15,26 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(update_params)
+      redirect_to product_path
     else
-
+      flash.now[:alert] = '更新できませんでした。'
+      render :edit
+    end
   end
 
   private
 
   def product_params
+    params.require(:product).permit(:name,:price,:dealing_status,:product_introduction,:category_id,:product_condition_id,:size_id,brand:[:brand],shipment:[:area,:charge_payment,:day],product_imgs:[:image])
+  end
+
+  def update_params
     params.require(:product).permit(:name,:price,:dealing_status,:product_introduction,:category_id,:product_condition_id,:size_id,brand:[:brand],shipment:[:area,:charge_payment,:day],product_imgs:[:image])
   end
   
